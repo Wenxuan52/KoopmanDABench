9501308be98ffc9ab375b3eda25ed0ef926c50dc
Wenxuan Yuan <wenxuan.yuan@qq.com>
2026-02-21 08:12:57 +0000

    update

--- scripts/run_ipynb.sh
+++ scripts/run_ipynb.sh
@@ -1,18 +1,17 @@
 #!/bin/bash -l
 
 #SBATCH --job-name=run_ipynb
-#SBATCH --partition=dgxl_irp
-#SBATCH --qos=dgxl_irp_low
-#SBATCH --cpus-per-task=4
+#SBATCH --partition=root
+#SBATCH --qos=intermediate
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=40G
-#SBATCH --time=24:00:00
+#SBATCH --time=06:00:00
 #SBATCH -e run_ipynb.err
 #SBATCH -o run_ipynb.out
 
-source /scratch_dgxl/wy524/miniconda3/etc/profile.d/conda.sh
-conda activate irp-env
+source /scratch_root/wy524/miniconda3/etc/profile.d/conda.sh
+conda activate koopmanda
 
-# Cylinder / KMG
-cd src/models/CAE_Linear/KMG
+cd docs/
 
-jupyter nbconvert --to notebook --execute --inplace kol_DA_randob.ipynb --ExecutePreprocessor.timeout=86400
+conda run -n koopmanda jupyter nbconvert --to notebook --execute --inplace ERA5_Example.ipynb --ExecutePreprocessor.timeout=86400
